b07cb580c81068900ba46e2796f542aaffcffafd
Carles <carlesrojas@outlook.com>
2021-08-06 19:54:50 +0200

    Open React Project in the Webview

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,10 @@
 # Change Log
 
 All notable changes to the "git-go" extension will be documented in this file.
-
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+## [0.0.1] - 2021-08-06
 
-- Initial release
+### Added
+
+-   Initial Release.
--- extension.js
+++ extension.js
@@ -1,4 +1,6 @@
 const vscode = require("vscode");
+const path = require("path");
+const nanoid = require("nanoid");
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -34,12 +36,16 @@ function activate(context) {
 // Creates and returns the Git Go panel
 function createGitGoPanel(context) {
     // Create Panel
-    var newPanel = vscode.window.createWebviewPanel("gitgo", "Git Go", getCurrentActiveColumn, {
+    var newPanel = vscode.window.createWebviewPanel("react", "Git Go", getCurrentActiveColumn, {
+        // Enable javascript in the webview
         enableScripts: true,
+
+        // And restric the webview to only loading content from our build directory.
+        localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, "build"))],
     });
 
     // Set panel HTML content
-    newPanel.webview.html = getWebviewContent();
+    newPanel.webview.html = getWebviewContent(context.extensionPath);
 
     // Reset when the current panel is closed
     newPanel.onDidDispose(
@@ -65,18 +71,43 @@ function openDevTools() {
     }, 500);
 }
 
-function getWebviewContent() {
+// Get the initial html content
+function getWebviewContent(extensionPath) {
+    const manifest = require(path.join(extensionPath, "build", "asset-manifest.json"));
+    const mainScript = manifest["files"]["main.js"];
+    const mainStyle = manifest["files"]["main.css"];
+    const scriptPathOnDisk = vscode.Uri.file(path.join(extensionPath, "build", mainScript));
+    const scriptUri = scriptPathOnDisk.with({ scheme: "vscode-resource" });
+    const stylePathOnDisk = vscode.Uri.file(path.join(extensionPath, "build", mainStyle));
+    const styleUri = stylePathOnDisk.with({ scheme: "vscode-resource" });
+
+    // Use a unique ID to whitelist which scripts can be run
+    const uniqueId = getUniqueId();
+
     return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Git Go</title>
-  </head>
-  <body>
-	  <p>Hello Git </p>
-  </body>
-  </html>`;
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" />
+                    <meta name="theme-color" content="#000000" />
+                    <meta name="description" content="Manage git projects with a simple UI." />
+                    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${uniqueId}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
+                    <link rel="stylesheet" type="text/css" href="${styleUri}">
+                    <title>Git Go</title>
+                    <base href="${vscode.Uri.file(path.join(extensionPath, "build")).with({ scheme: "vscode-resource" })}/">
+                </head>
+                <body>
+                    <noscript>You need to enable JavaScript to run this app.</noscript>
+                    <div id="root"></div>
+                    <script nonce="${uniqueId}" src="${scriptUri}"></script>
+                </body>
+            </html>`;
+}
+
+// Get unique ID
+function getUniqueId() {
+    const generateID = nanoid.customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 32);
+    return generateID();
 }
 
 // this method is called when your extension is deactivated
