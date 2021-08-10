b8539355421f8ad3b656c2878176ef952e76ccb7
CarlesRojas <carlesrojas@outlook.com>
2021-08-10 20:28:58 +0200

    Git interface setup

--- extension-src/webviewInterface.js
+++ extension-src/webviewInterface.js
@@ -20,18 +20,18 @@ class WebviewInterface {
 
         if (!("type" in message)) return;
 
-        switch (message.type) {
-            case "getGitUserAndEmail":
-                this.getGitUserAndEmail(message);
-                break;
-
-            case "setGitUserAndEmail":
-                this.setGitUserAndEmail(message);
-                break;
-
-            default:
-                break;
-        }
+        if (message.type === "getGitUserAndEmail") this.getGitUserAndEmail(message);
+        else if (message.type === "setGitUserAndEmail") this.setGitUserAndEmail(message);
+        else if (message.type === "initRepo") this.initRepo(message);
+        else if (message.type === "cloneRepo") this.cloneRepo(message);
+        else if (message.type === "getRemote") this.getRemote(message);
+        else if (message.type === "setRemote") this.setRemote(message);
+        else if (message.type === "stage") this.stage(message);
+        else if (message.type === "unstage") this.unstage(message);
+        else if (message.type === "commit") this.commit(message);
+        else if (message.type === "branch") this.branch(message);
+        else if (message.type === "checkout") this.checkout(message);
+        else if (message.type === "merge") this.merge(message);
     }
 
     //  ###########################################################
@@ -52,8 +52,93 @@ class WebviewInterface {
     async setGitUserAndEmail({ user, email }) {
         if (!user || !email) return;
 
-        await spawnGit(["config", "--global", "user.name", user]);
-        await spawnGit(["config", "--global", "user.email", email]);
+        const nameResponse = await spawnGit(["config", "--global", "user.name", user]);
+        const emailResponse = await spawnGit(["config", "--global", "user.email", email]);
+
+        // ROJAS ADD THIS TO ALL CALLS
+        if (nameResponse.length || emailResponse.length) this.sendMessage({ type: "error", message: "Error setting the name or email." });
+    }
+
+    //  ###########################################################
+    //      INIT
+    //  ###########################################################
+
+    // Init a repositori in this directory
+    async initRepo() {
+        await spawnGit(["init"]);
+    }
+
+    // Clone a repository
+    async cloneRepo(url) {
+        if (!url) return;
+
+        await spawnGit(["clone", url]);
+    }
+
+    // Get the repo remote
+    async getRemote() {
+        const remote = await spawnGit(["config", "--get", "remote.origin.url"]);
+
+        if (!remote.length) this.sendMessage({ type: "remoteNotConfigured" });
+
+        this.sendMessage({ type: "remote", remote: remote[0] });
+    }
+
+    // Set the repo remote
+    async setRemote(url) {
+        if (!url) return;
+
+        await spawnGit(["remote", "add", "origin", url]);
+    }
+
+    //  ###########################################################
+    //      STAGE & COMMIT
+    //  ###########################################################
+
+    // Stage the files
+    async stage(files) {
+        if (!files.length) return;
+
+        await spawnGit(["add", ...files]);
+    }
+
+    // Unstage the files
+    async unstage(files) {
+        if (!files.length) return;
+
+        await spawnGit(["reset", ...files]);
+    }
+
+    // Commit staged files
+    async commit(message) {
+        if (!message) return;
+
+        await spawnGit(["commit", "-m", message]);
+    }
+
+    //  ###########################################################
+    //      BRANCHES & MERGE
+    //  ###########################################################
+
+    // Create new branch
+    async branch(branchName, hash) {
+        if (!branchName || !hash) return;
+
+        await spawnGit(["branch", branchName, hash]);
+    }
+
+    // Checkout branch
+    async checkout(branchName) {
+        if (!branchName) return;
+
+        await spawnGit(["checkout", branchName]);
+    }
+
+    // Merge branch to current one
+    async merge(branchName) {
+        if (!branchName) return;
+
+        await spawnGit(["merge", branchName]);
     }
 }
 
